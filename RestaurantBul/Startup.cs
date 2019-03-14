eee48eeb444c871bfd21b29823d039cafc268882
ozlembiyik <ozlemalbyk@gmail.com>
2019-03-14 14:50:05 +0300

    ilk

---
+++ RestaurantBul/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantBul.Startup))]
+namespace RestaurantBul
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
